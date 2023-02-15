0a5b4cef69e2c0937171978086b8ac1418f34ec8
lleytonito <111662027+lleytonito@users.noreply.github.com>
2023-02-15 11:19:46 -0700

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,30 @@
+#include "program1.h"
+
+// Loads the grammar.
+void LoadGrammar(char *grammarFileName) {
+}
+
+// Returns a string with some statistics for the grammar.
+char* GetStats() {
+	return "";
+}
+
+// Returns the first sets of all nonterminals.
+char* GetFirstSets() {
+	return "";
+}
+
+// Returns the first set of the specified nonterminal.
+char* GetFirstSet(char *nonTerminal) {
+	return "";
+}
+
+// Returns the follow sets of all nonterminals.
+char* GetFollowSets() {
+	return "";
+}
+
+// Returns the follow set of the specified nonterminal.
+char* GetFollowSet(char *nonTerminal) {
+	return "";
+}
